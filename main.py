bed5f8159bbce525e780321dc7a0efd952e9e67c
Jelle Rodenburg <jellerodenburg@gmail.com>
2022-02-01 14:39:15 +0100

    Update index.html text

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route("/")
 def index():
-    return "Hello, this is the CD test app!"
+    return "Hello, this is the CD test app! I updated this text."
 
 
 @app.route("/about")

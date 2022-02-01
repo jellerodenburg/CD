43010c73ec36b53c469f30ac2d1d58993cdf4c0c
Jelle Rodenburg <jellerodenburg@gmail.com>
2022-02-01 11:26:02 +0100

    Add Flask app files

---
+++ main.py
@@ -0,0 +1,30 @@
+# Import what we need from flask
+from flask import Flask, redirect, render_template
+
+# Create a Flask app inside `app`
+app = Flask(__name__)
+
+# Assign a function to be called when the path `/` is requested
+@app.route("/")
+def index():
+    return "Hello, world!"
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+@app.route("/cow")
+def cow():
+    return "MOoooOo!"
+
+
+@app.route("/home")
+def home():
+    return redirect("/", code="302")
+
+
+@app.route("/links")
+def links():
+    return render_template("links.html", title="Links")
